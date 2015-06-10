92a0587dc52f6a761028fe3d5d0ac494ae29a7b8
sethvincent <sethvincent@gmail.com>
2015-06-09 19:16:04 -0700

    update level-simple-indexes

--- index.js
+++ index.js
@@ -31,10 +31,8 @@ function LevelModel (db, opts) {
 
   opts.schema.required = opts.schema.required.concat('key')
   this.validateOpts = opts.validateOpts
-
   this.schema = opts.schema
   this.validate = validator(opts.schema, opts.validateOpts)
-  this.indexes = {}
 
   function map (key, callback) {
     self.get(key, function (err, val) {
