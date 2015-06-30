9eb4032b3df87e9736fb457ca364d310c8254662
sethvincent <sethvincent@gmail.com>
2015-06-30 11:17:50 -0700

    only concat key if not already required

--- index.js
+++ index.js
@@ -29,7 +29,8 @@ function LevelModel (db, opts) {
     type: 'object'
   }, schema)
 
-  opts.schema.required = (opts.schema.required || []).concat('key')
+  opts.schema.required = opts.schema.required || []
+  if (opts.schema.require.indexOf('key') < 0) opts.schema.required.concat('key')
   this.validateOpts = opts.validateOpts
   this.schema = opts.schema
   this.validate = validator(opts.schema, opts.validateOpts)
